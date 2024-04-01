f68a70cc9534b54a4a3f74846124fe7b9dd1e150
LeoTeodoro <leoteodoro18@gmail.com>
2024-04-01 20:15:38 -0300

    :card_file_box: raw: exercício de  exemplo de prova

---
+++ exemploProva.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+arr = np.loadtxt('paises.csv', delimiter=';', dtype=str, encoding='utf-8')
+
+#1)
+print(arr[1:,:4])
+
+#2)
+arrRegion = np.unique(arr[1:,1])
+arrRegionSize = arrRegion.size
+print("Numero de regiões diferentes:",arrRegionSize)
+print(arrRegion)
+
+#3)
+literacyFloat = arr[1:, 9].astype(float)
+literacySum = np.sum(literacyFloat)
+totalCoutries = arr[1:, 0].size
+print("Média de alfabetização:",literacySum/totalCoutries, "%")
+
+#4)
+arrNorthAmerica = np.char.count(arr[1:,1], 'NORTHERN AMERICA')
+print("Numero de países na América do Norte:",np.count_nonzero(arrNorthAmerica))
+
+#5)
+arrSouthAmericaAndCaribean = arr[arr[:,1] == 'LATIN AMER. & CARIB    ']
+print(np.max(arrSouthAmericaAndCaribean[1:,8].astype(float)))
+
