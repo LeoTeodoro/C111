fcd9a41bc451b768bad82ea18556a2211006be07
LeoTeodoro <leoteodoro18@gmail.com>
2024-05-26 12:26:05 -0300

    retirando comentarios

--- cap5/cap5exerc.py
+++ cap5/cap5exerc.py
@@ -3,20 +3,20 @@ import numpy as np
 
 df = pd.read_csv('cap5/paises.csv', delimiter=';')
 
-#1. a) 
-# oceania = df[df['Region'].str.contains('OCEANIA')]
-# print(oceania['Country'])
+# 1. a) 
+oceania = df[df['Region'].str.contains('OCEANIA')]
+print(oceania['Country'])
 # #b)
-# print(oceania['Country'].count())
+print(oceania['Country'].count())
 
 #2
-# meanLiteracy = df['Literacy (%)'].mean()
-# print(meanLiteracy)
+meanLiteracy = df['Literacy (%)'].mean()
+print(meanLiteracy)
 
 #3
-# maxPop = df['Population'].max()
-# countryMaxPop = df[df['Population'] == maxPop]
-# print(countryMaxPop.loc[:,['Country','Region']])
+maxPop = df['Population'].max()
+countryMaxPop = df[df['Population'] == maxPop]
+print(countryMaxPop.loc[:,['Country','Region']])
 
 #4
 noCoastCountry = df[df['Coastline (coast/area ratio)'] == 0]
