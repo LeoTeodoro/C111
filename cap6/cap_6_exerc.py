49ba90d9119298f6c5c7eecce25e4c8bbde7782e
LeoTeodoro <leoteodoro18@gmail.com>
2024-06-06 14:40:37 -0300

    questão 2 do capitulo 6

--- cap6/cap_6_exerc.py
+++ cap6/cap_6_exerc.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
+#1
 df = pd.read_csv('cap6/space.csv', delimiter=';')
 
 # Filtrando as empresas dos EUA e da China
@@ -21,4 +23,22 @@ plt.bar(countries, counts)
 plt.xlabel('Country')
 plt.ylabel('Number of Companies')
 plt.title('Number of Space Companies in USA and China')
+plt.show()
+
+#2
+df2 = pd.read_csv('cap6/paises.csv', delimiter=';')
+
+#Separando os países da américa do norte
+north_america = df2[df2['Region'].str.contains('NORTHERN AMERICA')]
+
+x = north_america['Country']
+y = north_america['Deathrate']
+y2 = north_america['Birthrate']
+
+plt.xlabel('Países')
+plt.ylabel('Deathrate (verde) e Birthrate (azul)')
+# PLOT
+plt.plot(x, y, 'o--g',
+         x, y2, 'o--b',
+         linewidth=1.5, markersize=5)
 plt.show()
